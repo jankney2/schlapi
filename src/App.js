c41be1a4d63c842686bfd416ac2082ee802ea984
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-2.local>
2019-11-14 19:35:11 -0700

    not working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import gql from "gql-tag";
+import { useQuery, ApolloProvider, useApolloClient }  from "@apollo/react-hooks";
+import "./App.css";
+
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+});
+
+const c2=useApolloClient()
 
 function App() {
-  return (
-    <div className="App">
-      <h1>
-        get a rick and morty character 
-      </h1>
 
-<input placeholder='search by name'></input>
-    </div>
+  const {loading, err, data}=useQuery(getAll)
+  const getAll = gql`
+    {
+      characters(page: 1, filter: { name: "rick" }) {
+        results {
+          name
+          status
+          image
+        }
+      }
+    }
+  `;
+
+  useEffect(() => {
+
+  }, []);
+
+  if(err){
+    return(
+      <div>error</div>
+    )
+  }
+  if(loading){
+    return(
+      <div>loading</div>
+    )
+  }
+  if(data){
+    return(
+      <div>data</div>
+    )
+  }
+
+  return (
+    <ApolloProvider client={client}>
+
+
+
+    </ApolloProvider>
   );
 }
+
+
 
 export default App;
